70eb9d31af09eb7bf04ad023ac69520fc4ed5309
philip baya mweri <dukebaya@gmail.com>
2023-05-24 21:11:44 +0300

    update

--- duke1.py
+++ duke1.py
@@ -1,3 +1,21 @@
 print("hello world!")
 Year = 1991
 print(type)
+Year = 1991
+print(type(Year))
+print(Year)
+
+Year=(1991)
+print(type(Year))
+
+print("students")
+Year ="1991"
+Year
+print(type(Year))
+
+Year =("1991")
+print(type(Year), Year)
+
+Movies=["citadel","vikings","taken","war","friends"]
+print(Movies)
+
